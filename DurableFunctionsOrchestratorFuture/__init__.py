90dad17f36dd4a7381984ad7c25cecca86dda5c8
micoconnell <105730018+micoconnell@users.noreply.github.com>
2023-04-17 15:11:17 -0600

    Main

---
+++ DurableFunctionsOrchestratorFuture/__init__.py
@@ -0,0 +1,42 @@
+import logging
+import json
+import datetime
+import azure.functions as func
+import azure.durable_functions as df
+
+def orchestrator_function(context: df.DurableOrchestrationContext):
+    timeout = context.current_utc_datetime + datetime.timedelta(minutes=1)
+    column_apis = [
+        [80, 79, 78, 322680, 322669, "Future1.csv"],
+        [293674, 322687, 293675, 228, "Future2.csv"],
+        [84, 322665, 294451, 294452, 294427, "Future3.csv"],
+        [294428, 294418, 294417, "Future4.csv"],
+        [294442, 294441, "Future5.csv"],
+        [226, 225, 86, 85, 322684, "Future6.csv"],
+        [87, 23695, 322677, 23694, "Future7.csv"],
+        [224, 350464, 350466, 350465, 350463, "Future8.csv"],
+        [337425, 337426, "Future9.csv"]
+    ]
+
+    current_db_results = []
+
+    for column_api in column_apis:
+        current_db_task = context.call_activity('CurrentPROD', column_api)
+        timeout_task = context.create_timer(timeout)
+        winning_task = yield context.task_any([current_db_task, timeout_task])
+
+        if winning_task == timeout_task:
+            raise Exception("Orchestration timed out.")
+        
+        current_db_result = current_db_task.result
+        current_db_results.append(current_db_result)
+
+        # Add a 1 second wait between each call to HistoricalDBPROD
+        yield context.create_timer(context.current_utc_datetime + datetime.timedelta(seconds=4))
+
+    Grouping_Task = context.call_activity('CurrentGrouping', "DONOW")
+    Grouping_Results = yield context.task_all([Grouping_Task])
+
+    
+
+main = df.Orchestrator.create(orchestrator_function)
